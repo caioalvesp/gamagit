818c382776e210ee50544d436edfe3d5cfa6525e
caioalvesp <caiovictoralvesp@gmail.com>
2021-07-21 22:15:44 -0300

    Criação da aplicação

---
+++ src/routes.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import { Switch, Route, BrowserRouter } from 'react-router-dom';
+
+import Repositories from './pages/Repositories';
+import Home from './pages/Home';
+
+export default function Routes() {
+  return (
+    <BrowserRouter>
+      <Switch>
+        <Route exact path='/' component={Home} />
+        <Route path='/repositories' component={Repositories} />
+      </Switch>
+    </BrowserRouter>
+  )
+}
